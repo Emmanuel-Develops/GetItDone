c49ba000ebec3d960338358bd413b8adc496a528
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-15 06:05:45 +0100

    Code refactor
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
-let items = [];
-let workItems = [];
-let miscellaneous= [];
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+const miscellaneous= [];
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    let today = new Date();
-    let options ={
-        weekday: 'long',
-        day:'numeric', 
-        month : 'long'
-    };
 
-    let day = today.toLocaleDateString('en-US', options);
+app.get('/', function(req, res){
+
+   const day = date.getDate();
+
     res.render('list', { listTitle : day, newListItems : items});
 })
 
 app.post('/', function(req, res){
-    let item = req.body.item;
+    const item = req.body.item;
 
     //Differentiate task into categories
 
