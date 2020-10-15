090248a89e22a9b1120f69e5eaf2cc219812c443
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-15 05:16:22 +0100

    Testing ejs templates and implement logic for separating categories
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-let items = [ ];
+let items = [];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+
 app.get('/', function(req, res){
     let today = new Date();
     let options ={
@@ -16,13 +18,33 @@ app.get('/', function(req, res){
     };
 
     let day = today.toLocaleDateString('en-US', options);
-    res.render('list', {kindOfDay : day, newListItems : items});
+    res.render('list', { listTitle : day, newListItems : items});
 })
 
 app.post('/', function(req, res){
     let item = req.body.item;
-    items.push(item);
-    res.redirect('/');
+
+    //Differentiate task into categories
+
+    let listType = req.body.list;
+    console.log("List type is:", listType);
+
+    if(listType=== 'Work'){
+        workItems.push(item);
+        res.redirect('/work');
+    }
+    else{
+        items.push(item);
+        res.redirect('/');  
+    }
+})
+
+app.get('/work', function(req, res){
+    res.render('list', {listTitle: 'Work List', newListItems : workItems});
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
 })
 
 app.listen(3000, function(){
