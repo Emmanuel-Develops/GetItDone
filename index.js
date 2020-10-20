b01b6948614e9b53c3d68b3105dbd61a6a197280
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-20 04:57:01 +0100

    Set up app to use mongoose
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 
 const app = express();
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser : true, useUnifiedTopology : true});
 
 app.get('/', function(req, res){
 
