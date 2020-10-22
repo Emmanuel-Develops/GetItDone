2cc80ed2a5154d6de545238525547b572178cd31
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-22 18:56:27 +0100

    Connect to mongodb server
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify: false });
+mongoose.connect("mongodb+srv://karen:supersecret111@cluster0.prd3j.mongodb.net/todolistDB", { useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify: false });
 
 //Items Schema
 const itemsSchema = {
